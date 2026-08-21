45b63a6bff170d51884532247194c13fe0f050a6
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2026-08-21 12:27:15 +0400

    [AEGISORA-68167495] Implemented check empty.

--- src/EmptinessRuleGuardian.php
+++ src/EmptinessRuleGuardian.php
@@ -2,7 +2,11 @@
 
 namespace Aegisora\RuleGuardians\EmptinessRuleGuardian;
 
+use Aegisora\Guardian\Exceptions\GuardianExecutingRuleException;
+use Aegisora\Guardian\Exceptions\GuardianValidationException;
 use Aegisora\Guardian\Guardian;
+use Aegisora\Rules\Emptiness\EmptyRule;
+use Throwable;
 
 class EmptinessRuleGuardian
 {
@@ -12,5 +16,18 @@ class EmptinessRuleGuardian
         Guardian $guardian
     ) {
         $this->guardian = $guardian;
+    }
+
+    /**
+     * @param mixed $value
+     * @throws GuardianExecutingRuleException
+     * @throws GuardianValidationException
+     * @throws Throwable
+     */
+    public function checkEmpty(
+        $value,
+        ?Throwable $exception = null
+    ): void {
+        $this->guardian->check($value, EmptyRule::create(), $exception);
     }
 }
